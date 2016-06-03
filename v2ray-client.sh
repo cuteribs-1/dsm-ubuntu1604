8dfb3c9c86e2f774404397e70658d3b88780cf2c
cuteribs <ericfine@yeah.net>
2016-06-03 23:47:48 +0800

    c6

--- v2ray-client.sh
+++ v2ray-client.sh
@@ -19,4 +19,24 @@ if [ ! -f "$INSTALL_FLAG" ]; then
 	touch $INSTALL_FLAG
 fi
 
+if [ $ENABLE_HTTP = "yes" ]; then
+	INSTALL_FLAG="/app/privoxy.installed"	
+	
+	if [ ! -f "$INSTALL_FLAG" ]; then
+		dpkg -i /app/libpopt0_1.16-10_amd64.deb
+		dpkg -i /app/cron_3.0pl1-128ubuntu2_amd64.deb
+		dpkg -i /app/logrotate_3.8.7-2ubuntu2_amd64.deb
+		dpkg -i /app/privoxy_3.0.24-1_amd64.deb
+		rm /etc/init.d/privoxy
+
+		PRIVOXY_CONF="/app/privoxy.conf"
+		echo listen-address 0.0.0.0:$HTTP_PORT > $PRIVOXY_CONF
+		echo forward-socks5 / localhost:$LOCAL_PORT . >> $PRIVOXY_CONF
+		
+		touch $INSTALL_FLAG
+	fi
+	
+	privoxy $PRIVOXY_CONF
+fi
+
 /app/v2ray/v2ray -config $CONF
